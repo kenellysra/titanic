18135b569652ae6f0ce7901ad1ad888d0732b259
kenellysra <kenellyalmeida@gmail.com>
2018-07-05 00:44:51 +0100

    version1

--- titanicbasicprediction.R
+++ titanicbasicprediction.R
@@ -2,7 +2,7 @@
 setwd("/home/kenelly/workspaces/titanic/")
 
 titanicdf <- read.csv("train.csv", header = T, na.strings = c(""), stringsAsFactors = T)
-
+str(titanicdf)
 #isolating and exploring the dependent variable
 survived <- titanicdf$Survived
 
@@ -19,9 +19,85 @@ testing <-survived[index]
 
 #Perish model - As the majority is "no", the perish model will predict all the values as "no"
 
-perishmodel <- rep("No", length(survived))
+perishmodel <- rep("No", length(testing))
 
 #Coin model - predict randomly between Yes or No
 
+coinmodel <- round(runif(length(testing), min = 0, max = 1))
+coinmodel <- factor(coinmodel, levels = c(0, 1), labels = c("No", "Yes"))
+length(perishmodel)
+length(testing)
+perishmodel <- factor(perishmodel, levels = c("No", "Yes"), labels = c("No", "Yes"))
+
+table(testing, perishmodel)
+table(testing, coinmodel)
+
+#cheking accurancy
+coinaccurancy <- 1 - mean(coinmodel!=testing)
+coinaccurancy
+perishaccurancy <- 1 - mean(perishmodel != testing)
+perishaccurancy
 
 
+prop.table(table(testing))
+
+
+perish <- c()
+coin <- c()
+
+#Evaluating 1000 models from 1000 samples
+
+for (i in 1:1000){
+    index <- sample(1:length(survived), length(survived)*0.25, replace = FALSE)
+    testing <- survived[index]
+    
+    coinmodel <- round(runif(length(testing), min = 0, max = 1))
+    coinmodel <- factor(coinmodel, levels = c(0,1), labels = c("No", "Yes"))
+    
+    coin[i] <- 1 - mean(coinmodel!=testing)
+    perish[i] <- 1-mean(perishmodel!=testing)
+}
+
+results <- data.frame(coin, perish)
+names(results) <- c("Coin Accuracy", "Perishes Accurancy")
+summary(results)
+
+library(ggplot2)
+library(reshape)
+
+ggplot(melt(results), mapping = aes(fill = variable, x = value)) + geom_density(alpha = .5)
+boxplot(results)
+
+
+#Model considering gender as independent variable
+gendermodel <- titanicdf[,c("Sex", "Survived")]
+gendermodel$Survived <- factor(gendermodel$Survived, levels = c(0, 1), labels = c("No", "Yes"))
+
+index <- sample(1:length(gendermodel$Survived), length(gendermodel$Survived)*.75, replace = FALSE)
+trainingdata <-gendermodel[index,]
+testingdata <-gendermodel[-index,]
+dim(gendermodel)[1]
+
+table(trainingdata$Survived, trainigdata$Sex)
+
+predictsurvival <- function(data){
+    model <- rep("No", length(data))
+    model[data$Sex == 'female'] <- "Yes"
+    model[data$Sex == 'male'] <- "No"
+    return(model)
+}
+
+women <- c()
+
+for (i in 1:1000) {
+    index <- sample(1:length(gendermodel), length(gendermodel)*.75, replace = FALSE)
+    testing <- gendermodel[-index,]
+    womenmodel <- predictsurvival(testing)
+    women[i] <- 1 - mean(womenmodel !=testingdata$Survived)
+    
+}
+
+women
+results$WomenAccurancy <- women
+names(results)<- c("Coin", "All Perish", "Women")
+boxplot(results)
